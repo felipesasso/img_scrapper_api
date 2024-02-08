c5b65ce235fb86adddd0b4fbe399dd6b8bd67f5a
Felipe Coral Sasso <felipecsasso@gmail.com>
2024-02-08 15:59:40 -0300

    Add docstring

--- db.py
+++ db.py
@@ -2,10 +2,42 @@ import sqlite3
 
 
 class ImagesDatabase:
-    def __init__(self, path):
+    """
+    A class to interact with an SQLite database for storing images.
+
+    Attributes:
+    path (str): The file path to the SQLite database.
+
+    Methods:
+    save_image(image: bytes) -> None:
+        Saves the given image data to the database.
+
+        Args:
+        image (bytes): The binary image data to be saved.
+
+        Returns:
+        None
+    """
+
+    def __init__(self, path: str):
+        """
+        Initializes the ImagesDatabase object with the specified database file path.
+
+        Args:
+        path (str): The file path to the SQLite database.
+        """
         self.path = path
 
-    def save_image(self, image):
+    def save_image(self, image: bytes) -> None:
+        """
+        Saves the given image data to the database.
+
+        Args:
+        image (bytes): The binary image data to be saved.
+
+        Returns:
+        None
+        """
         try:
             conn = sqlite3.connect(self.path)
             cursor = conn.cursor()
--- img_scrapper_api.py
+++ img_scrapper_api.py
@@ -9,13 +9,39 @@ urllib3.disable_warnings()
 
 
 class ImageScrapperApi:
+    """A class for scraping images from the web and managing them in a database.
+
+    Attributes:
+        BASE_URL (str): The base URL of the website to scrape images from.
+        SEARCH_URL (str): The URL for conducting search queries.
+        images_db (ImagesDatabase): An instance of the ImagesDatabase class for managing images in the database.
+
+    Methods:
+        _get_page: Fetches a page with search results for a given query.
+        save_image: Saves an image to the database.
+        download_image: Downloads an image from a given URL.
+        get_images_urls: Retrieves a list of image URLs related to a search query.
+
+    """
+
     BASE_URL = "https://www.freeimages.com"
     SEARCH_URL = f"{BASE_URL}/search"
 
     def __init__(self):
+        """Initializes an ImageScrapperApi object."""
         self.images_db = ImagesDatabase("images_db.db")
 
     def _get_page(self, search_input, page):
+        """Fetches a page with the results of a search for the given input.
+
+        Args:
+            search_input (str): The string to be searched.
+            page (int): The page number of the search results.
+
+        Returns:
+            str: The HTML content of the page, or None if an error occurs.
+
+        """
         url = f"{self.SEARCH_URL}/{search_input}/{page}"
         try:
             response = requests.get(url, verify=False)
@@ -26,9 +52,27 @@ class ImageScrapperApi:
             return None
 
     def save_image(self, image):
+        """Saves the given image to the database.
+
+        Args:
+            image (bytes): The image data to be saved.
+
+        Returns:
+            None
+
+        """
         self.images_db.save_image(image)
 
     def download_image(self, image_url):
+        """Downloads an image from the given URL.
+
+        Args:
+            image_url (str): The URL of the image to be downloaded.
+
+        Returns:
+            bytes: The image data, or None if an error occurs.
+
+        """
         try:
             response = requests.get(image_url, verify=False)
             response.raise_for_status()
@@ -38,6 +82,16 @@ class ImageScrapperApi:
             return None
 
     def get_images_urls(self, search_input, max_number_of_images):
+        """Returns a list of URLs to images related to the given search input.
+
+        Args:
+            search_input (str): The string to be searched.
+            max_number_of_images (int): The maximum number of image URLs to be returned.
+
+        Returns:
+            List[str]: A list of URLs to images.
+
+        """
         page = 1
         images_url_list = []
         while len(images_url_list) < max_number_of_images:
